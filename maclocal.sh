1e1b26cc40d400a62523f0a74f0efd9a10e0458b
Thomas Klijnsma <tklijnsm@gmail.com>
2020-03-29 11:39:01 -0500

    more locations

--- maclocal.sh
+++ maclocal.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 source /etc/profile
+source ~/scripts/src/mac.sh
 
-application="open"
+application="mac"
 location=""
 file=""
 testmode="false"
+mnt_dir="/Users/klijnsma"
 
 print_usage() {
     printf "Usage: ..."
@@ -38,17 +40,26 @@ echo "macbook: remote file/dir = ${file}"
 
 remote_to_local(){
     if [ "${location}" == "zwolle" ]; then
-        file_local="/Users/thomas/mnt/zwolle${file}"
+        file_local="${mnt_dir}/mnt/zwolle${file}"
     elif [ "${location}" == "geneve" ]; then
-        file_local="/Users/thomas/mnt/rpi${file}"
+        file_local="${mnt_dir}/mnt/rpi${file}"
     elif [ "${location}" == "gcloud_cpu" ]; then
-        file_local="${file/\/home\/thomas//Users/thomas/mnt/gcloud-cpu}"
+        file_local="${file/\/home\/thomas/${mnt_dir}/mnt/gcloud-cpu}"
     elif [ "${location}" == "gcloud_gpu" ]; then
-        file_local="${file/\/home\/thomas//Users/thomas/mnt/gcloud-gpu}"
+        file_local="${file/\/home\/thomas/${mnt_dir}/mnt/gcloud-gpu}"
     elif [ "${location}" == "lpc" ]; then
-        file_local="${file/\/uscms\/home\/klijnsma//Users/thomas/mnt/lpc}"
+        file_local="${file}"
+        file_local="${file_local/\/uscms\/home\/klijnsma\/nobackup/${mnt_dir}/mnt/lpcnobackup}"
+        file_local="${file_local/\/uscms_data\/d3\/klijnsma/${mnt_dir}/mnt/lpcnobackup}"
+        file_local="${file_local/\/uscms_data\/d2\/klijnsma/${mnt_dir}/mnt/lpcnobackup}"
+        file_local="${file_local/\/uscms\/home\/klijnsma/${mnt_dir}/mnt/lpc}"
+        file_local="${file_local/\/uscms\/homes\/k\/klijnsma/${mnt_dir}/mnt/lpc}"
     elif [ "${location}" == "t3" ]; then
-        file_local="${file/\/mnt\/t3nfs01\/data01\/shome\/tklijnsm//Users/thomas/mnt/psi}"
+        file_local="${file/\/mnt\/t3nfs01\/data01\/shome\/tklijnsm/${mnt_dir}/mnt/psi}"
+    elif [[ $location == gc* ]]; then
+        file_local="${mnt_dir}/mnt/${location}${file}"
+    elif [ "${location}" == "rpi" ]; then
+        file_local="${mnt_dir}/mnt/rpi/${file}"
     else
         echo "Location ${location} has no mapping rule!"
         exit
